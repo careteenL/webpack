a59b7c8831c09e89e503c42a4c98fdba50db8c40
careteenL <15074806497@163.com>
2019-01-16 15:45:52 +0800

    examples: add my-simple case

---
+++ examples/my-simple/webpack.config.js
@@ -0,0 +1,9 @@
+let path = require('path')
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
